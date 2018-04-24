01e723b2a914bbfd830e58eadc3f9ee7ce3b325d
BrayanGBejarano <brayan.bejarnao17@hotmail.com>
2018-04-24 14:24:16 -0500

    corrected interfaz

--- ProyectoKardex/src/kardex/view/KardexView.java
+++ ProyectoKardex/src/kardex/view/KardexView.java
@@ -16,22 +16,18 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import kardex.logic.ILogicKardex;
 
-public class KardexView {
-
-	private ILogicKardex logicKardex;
-
 @SuppressWarnings("serial")
-public class Principal extends JFrame implements ActionListener{
-	
+public class KardexView extends JFrame implements ActionListener {
+
 	private PanelKardex panelKardex;
-	
+
 	private VentanaAgregar ventanaAgregar;
-	
+
 	private PanelOpcionesAgregar panelBotones;
-	
+
 	private String tipo;
-	
-	public Principal() {
+
+	public KardexView() {
 		panelKardex = new PanelKardex(this);
 		ventanaAgregar = new VentanaAgregar(this);
 		tipo = "";
@@ -41,80 +37,76 @@ public class Principal extends JFrame implements ActionListener{
 		add(panelKardex, BorderLayout.CENTER);
 		pack();
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String comando = e.getActionCommand();
-		if(comando.equals("A")) {
-			if(tipo.equals("I")) {
-				panelKardex.addInventarioInicial(ventanaAgregar.darUnidades(), ventanaAgregar.darPrecioUnidad(), ventanaAgregar.darTotal());
+		if (comando.equals("A")) {
+			if (tipo.equals("I")) {
+				panelKardex.addInventarioInicial(ventanaAgregar.darUnidades(), ventanaAgregar.darPrecioUnidad(),
+						ventanaAgregar.darTotal());
 				this.setVisible(true);
 				ventanaAgregar.setVisible(false);
-			}else if(tipo.equals("V")) {
-				
-			}else if(tipo.equals("C")) {
-				
+			} else if (tipo.equals("V")) {
+
+			} else if (tipo.equals("C")) {
+
 			}
-		}else if(comando.equals("C")) {
+		} else if (comando.equals("C")) {
 			this.setVisible(true);
 			ventanaAgregar.setVisible(false);
 			ventanaAgregar.limpiar();
-		}else if(comando.equals("In")) {
+		} else if (comando.equals("In")) {
 			this.setVisible(false);
 			tipo = "I";
 			ventanaAgregar.setVisible(true);
-		}else if(comando.equals("VEN")) {
+		} else if (comando.equals("VEN")) {
 			this.setVisible(false);
 			tipo = "V";
 			ventanaAgregar.setVisible(true);
-		}else if(comando.equals("COM")) {
+		} else if (comando.equals("COM")) {
 			this.setVisible(false);
 			tipo = "C";
 			ventanaAgregar.setVisible(true);
 		}
 	}
-	
-	public static void main(String[] args) {
-		Principal principal = new Principal();
-		principal.setVisible(true);
-	}
 }
 
 @SuppressWarnings("serial")
-class PanelBotones extends JPanel{
-	
+class PanelBotones extends JPanel {
+
 	private JButton botonAgregar;
-	
+
 	private JButton botonCancelar;
 
-	private Principal principal;
-	
-	public PanelBotones(Principal p) {
+	private KardexView principal;
+
+	public PanelBotones(KardexView p) {
 		principal = p;
-		botonAgregar =  new JButton("Aceptar");
+		botonAgregar = new JButton("Aceptar");
 		botonAgregar.addActionListener(principal);
 		botonAgregar.setActionCommand("A");
 		botonCancelar = new JButton("Cancelar");
 		botonCancelar.addActionListener(principal);
 		botonCancelar.setActionCommand("C");
-		setLayout(new GridLayout(1,2));
+		setLayout(new GridLayout(1, 2));
 		add(botonAgregar);
 		add(botonCancelar);
-	}	
+	}
 }
 
 @SuppressWarnings("serial")
-class VentanaAgregar extends JFrame{
-	private JLabel [] etiquetas;
-	
+class VentanaAgregar extends JFrame {
+	private JLabel[] etiquetas;
+
 	private JTextField[] cajasTexto;
-	
+
 	@SuppressWarnings("unused")
-	private Principal principal;
-	
+	private KardexView principal;
+
 	private PanelBotones panelBotones;
-	
-	public VentanaAgregar(Principal p) {
+
+	public VentanaAgregar(KardexView p) {
 		principal = p;
 		etiquetas = new JLabel[3];
 		cajasTexto = new JTextField[3];
@@ -127,13 +119,13 @@ class VentanaAgregar extends JFrame{
 	}
 
 	public void limpiar() {
-		cajasTexto [0].setText("");
-		cajasTexto [1].setText("");
-		cajasTexto [2].setText("");
+		cajasTexto[0].setText("");
+		cajasTexto[1].setText("");
+		cajasTexto[2].setText("");
 		validate();
 		repaint();
 	}
-	
+
 	public String darTotal() {
 		return cajasTexto[2].getText();
 	}
@@ -147,43 +139,40 @@ class VentanaAgregar extends JFrame{
 	}
 
 	private JPanel crearPanel() {
-		JPanel temp = new JPanel((new GridLayout(2,3)));
-		etiquetas [0] = new JLabel("Cantidad");
-		etiquetas [1] = new JLabel("precioXunidad");
-		etiquetas [2] = new JLabel("precio total");
-		cajasTexto [0] = new JTextField("");
-		cajasTexto [1] = new JTextField("");
-		cajasTexto [2] = new JTextField("");
-		
+		JPanel temp = new JPanel((new GridLayout(2, 3)));
+		etiquetas[0] = new JLabel("Cantidad");
+		etiquetas[1] = new JLabel("precioXunidad");
+		etiquetas[2] = new JLabel("precio total");
+		cajasTexto[0] = new JTextField("");
+		cajasTexto[1] = new JTextField("");
+		cajasTexto[2] = new JTextField("");
+
 		for (int i = 0; i < cajasTexto.length; i++) {
 			etiquetas[i].setHorizontalAlignment(SwingConstants.CENTER);
 			temp.add(etiquetas[i]);
 		}
-		
+
 		for (int i = 0; i < cajasTexto.length; i++) {
 			temp.add(cajasTexto[i]);
 		}
 		return temp;
 	}
-	
-	
+
 }
 
 @SuppressWarnings("serial")
-class PanelKardex extends JPanel{
-	 private JLabel[][] matriz;
-	 
-	 private int fila;
-	
-	 private Principal principal;
-	 
-	 public PanelKardex(Principal p) {
-		 llenarInicial();
-		 fila = 2;
-		 principal = p;
-	 }
+class PanelKardex extends JPanel {
+	private JLabel[][] matriz;
 
+	private int fila;
 
+	private KardexView principal;
+
+	public PanelKardex(KardexView p) {
+		llenarInicial();
+		fila = 2;
+		principal = p;
+	}
 
 	private void llenarInicial() {
 		matriz = new JLabel[20][10];
@@ -192,7 +181,7 @@ class PanelKardex extends JPanel{
 		for (int i = 0; i < matriz.length; i++) {
 			for (int j = 0; j < matriz[0].length; j++) {
 				matriz[i][j] = new JLabel("  ");
-				if(!primera) {
+				if (!primera) {
 					matriz[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
 				}
 			}
@@ -206,13 +195,13 @@ class PanelKardex extends JPanel{
 		matriz[0][5].setHorizontalAlignment(SwingConstants.CENTER);
 		matriz[0][8].setText("Saldo");
 		matriz[0][8].setHorizontalAlignment(SwingConstants.CENTER);
-		for (int i = 1; i < matriz[0].length; i+=3) {
+		for (int i = 1; i < matriz[0].length; i += 3) {
 			matriz[1][i].setText("und");
 		}
-		for (int i = 2; i < matriz[0].length; i+=3) {
+		for (int i = 2; i < matriz[0].length; i += 3) {
 			matriz[1][i].setText("$/und");
 		}
-		for (int i = 3; i < matriz[0].length; i+=3) {
+		for (int i = 3; i < matriz[0].length; i += 3) {
 			matriz[1][i].setText("$Total");
 		}
 		for (int i = 0; i < matriz.length; i++) {
@@ -221,47 +210,50 @@ class PanelKardex extends JPanel{
 			}
 		}
 	}
-	
+
 	public void addInventarioInicial(String unidades, String costoUnidad, String costoTotal) {
 		int filaActu = 0;
-		if(fila==2) {
+		if (fila == 2) {
 			try {
 				Integer.parseInt(unidades);
 				Double.parseDouble(costoUnidad);
 				Double.parseDouble(costoTotal);
 				matriz[fila][7].setText(unidades);
-				matriz[fila][8].setText(costoUnidad);	
+				matriz[fila][8].setText(costoUnidad);
 				matriz[fila][9].setText(costoTotal);
 				matriz[fila][0].setText("I.Inicial");
 				matriz[4][0].setText("compra # 1");
 				fila++;
-			}catch (Exception e) {
-				JOptionPane.showMessageDialog(principal, "información erronea!", "advertencia", JOptionPane.ERROR_MESSAGE);
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(principal, "información erronea!", "advertencia",
+						JOptionPane.ERROR_MESSAGE);
 			}
-		}else {
+		} else {
 			try {
-				if(matriz[fila-1][0].getText().equals("  ")||matriz[fila-1][0].getText().equals("I.Inicial")) {
+				if (matriz[fila - 1][0].getText().equals("  ") || matriz[fila - 1][0].getText().equals("I.Inicial")) {
 					boolean esta = false;
-					for (int i = fila-1; i >0 && !esta ; i--) {
-						if(matriz[i][8].getText().equals(costoUnidad)) {
+					for (int i = fila - 1; i > 0 && !esta; i--) {
+						if (matriz[i][8].getText().equals(costoUnidad)) {
 							esta = true;
 							filaActu = i;
 						}
 					}
-					if(esta) {
-						int unidadesActu =Integer.parseInt(matriz[filaActu][7].getText());
-						matriz[filaActu][7].setText((unidadesActu+Integer.parseInt(unidades))+"");
-						int costoTotalAct = Integer.parseInt(matriz[filaActu][7].getText())*Integer.parseInt(matriz[filaActu][8].getText());
-						matriz[filaActu][9].setText(costoTotalAct+"");
-					}else {
+					if (esta) {
+						int unidadesActu = Integer.parseInt(matriz[filaActu][7].getText());
+						matriz[filaActu][7].setText((unidadesActu + Integer.parseInt(unidades)) + "");
+						int costoTotalAct = Integer.parseInt(matriz[filaActu][7].getText())
+								* Integer.parseInt(matriz[filaActu][8].getText());
+						matriz[filaActu][9].setText(costoTotalAct + "");
+					} else {
 						matriz[fila][7].setText(unidades);
-						matriz[fila][8].setText(costoUnidad);	
+						matriz[fila][8].setText(costoUnidad);
 						matriz[fila][9].setText(costoTotal);
 						fila++;
 					}
 				}
-			}catch (Exception e) {
-				JOptionPane.showMessageDialog(principal, "información erronea!", "advertencia", JOptionPane.ERROR_MESSAGE);
+			} catch (Exception e) {
+				JOptionPane.showMessageDialog(principal, "información erronea!", "advertencia",
+						JOptionPane.ERROR_MESSAGE);
 			}
 		}
 		validate();
@@ -270,38 +262,40 @@ class PanelKardex extends JPanel{
 }
 
 @SuppressWarnings("serial")
-class PanelOpcionesAgregar extends JPanel{
+class PanelOpcionesAgregar extends JPanel {
 	private JButton botonInicial;
-	
+
 	private JButton botonCompra;
-	
+
 	private JButton botonVenta;
-	
-	private Principal principal;
-	
-	public PanelOpcionesAgregar(Principal p) {
-		
+
+	private KardexView principal;
+
+	public PanelOpcionesAgregar(KardexView p) {
+
 		principal = p;
-		
+
 		botonInicial = new JButton("A. Inicial");
 		botonInicial.addActionListener(principal);
 		botonInicial.setActionCommand("In");
-		
+
 		botonVenta = new JButton("A. Venta");
 		botonVenta.addActionListener(principal);
 		botonVenta.setActionCommand("VEN");
-		
+
 		botonCompra = new JButton("A. Compra");
 		botonCompra.setActionCommand("COM");
 		botonCompra.addActionListener(principal);
-		
+
 		setLayout(new GridLayout(1, 3));
-		
+
 		add(botonInicial);
 		add(botonCompra);
 		add(botonVenta);
 	}
-}
 
-
+	public static void main(String[] args) {
+		KardexView prin = new KardexView();
+		prin.setVisible(true);
+	}
 }
