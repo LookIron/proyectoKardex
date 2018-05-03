7fea77fa2b04c7332e82d776f6161310f65bcdbb
BrayanGBejarano <brayan.bejarnao17@hotmail.com>
2018-05-03 10:57:06 -0500

    excepciones manejadas

--- ProyectoKardex/src/kardex/view/VistaPEPS.java
+++ ProyectoKardex/src/kardex/view/VistaPEPS.java
@@ -1,28 +1,30 @@
 package kardex.view;
 
 import java.awt.BorderLayout;
+import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import kardex.logic.ILogicKardex;
 import kardex.logic.LogicKardex;
 
-public class VistaPEPS extends JFrame implements ActionListener{
+public class VistaPEPS extends JFrame implements ActionListener {
 
 	private PanelKardex panelKardex;
 
 	private FrameInfo ventanaAgregar;
 
 	private LogicKardex mundo;
-	
+
 	private ILogicKardex temp;
 
 	private PanelOpcionesAgregar panelBotones;
 
 	private String tipo;
-	
+
 	public VistaPEPS() {
 		panelKardex = new PanelKardex(this);
 		ventanaAgregar = new FrameInfo(this);
@@ -37,66 +39,102 @@ public class VistaPEPS extends JFrame implements ActionListener{
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		String comando = e.getActionCommand();
-		if (comando.equals("A")) {
-			if (tipo.equals("I")) {
-				panelKardex.addInformacion(ventanaAgregar.darUnidades(), ventanaAgregar.darPrecioUnidad(),
-						ventanaAgregar.darTotal(), 0, "","","","");
-				mundo.setSaldoUnidades(Double.parseDouble(ventanaAgregar.darUnidades()));
-				mundo.setSaldoValorUnidad(Double.parseDouble(ventanaAgregar.darPrecioUnidad()));
-				mundo.setSaldoValorTotal(Double.parseDouble(ventanaAgregar.darPrecioUnidad()));
-				temp = new LogicKardex(Double.parseDouble(ventanaAgregar.darUnidades()), Double.parseDouble(ventanaAgregar.darPrecioUnidad()), Double.parseDouble(ventanaAgregar.darPrecioUnidad()));
+		try {
+			String comando = e.getActionCommand();
+			if (comando.equals("A")) {
+				if (tipo.equals("I")) {
+					if (Double.parseDouble(ventanaAgregar.darUnidades()) <= 0
+							|| Double.parseDouble(ventanaAgregar.darPrecioUnidad()) <= 0
+							|| Double.parseDouble(ventanaAgregar.darPrecioUnidad()) <= 0) {
+						throw new Exception("Señor Stark... no me quiero ir....");
+					}
+					panelKardex.addInformacion(ventanaAgregar.darUnidades(), ventanaAgregar.darPrecioUnidad(),
+							ventanaAgregar.darTotal(), 0, "", "", "", "");
+					mundo.setSaldoUnidades(Double.parseDouble(ventanaAgregar.darUnidades()));
+					mundo.setSaldoValorUnidad(Double.parseDouble(ventanaAgregar.darPrecioUnidad()));
+					mundo.setSaldoValorTotal(Double.parseDouble(ventanaAgregar.darPrecioUnidad()));
+					temp = new LogicKardex(Double.parseDouble(ventanaAgregar.darUnidades()),
+							Double.parseDouble(ventanaAgregar.darPrecioUnidad()),
+							Double.parseDouble(ventanaAgregar.darPrecioUnidad()));
+					this.setVisible(true);
+					ventanaAgregar.setVisible(false);
+					ventanaAgregar.limpiar();
+				} else if (tipo.equals("V")) {
+					if (Double.parseDouble(ventanaAgregar.darUnidades()) <= 0
+							|| Double.parseDouble(ventanaAgregar.darPrecioUnidad()) <= 0
+							|| Double.parseDouble(ventanaAgregar.darPrecioUnidad()) <= 0) {
+						throw new Exception("Señor Stark... no me quiero ir....");
+					}
+					try {
+						int numero = 1;
+						double unidades = Double.parseDouble(ventanaAgregar.darUnidades());
+						double valorUnidades = Double.parseDouble(ventanaAgregar.darPrecioUnidad());
+						double valorTotal = Double.parseDouble(ventanaAgregar.darTotal());
+						mundo.registrarVenta(unidades, valorUnidades, valorTotal);
+						if (unidades > mundo.getSaldoUnidades()) {
+							throw new Exception();
+						}
+
+						panelKardex.addInformacion(ventanaAgregar.darUnidades(), ventanaAgregar.darPrecioUnidad(),
+								ventanaAgregar.darTotal(), numero, "V", mundo.getSaldoUnidades() + "",
+								mundo.getSaldoValorUnidad() + "", mundo.getSaldoValorTotal() + "");
+
+						numero++;
+						this.setVisible(true);
+						ventanaAgregar.setVisible(false);
+						ventanaAgregar.limpiar();
+					} catch (Exception e1) {
+						JOptionPane.showMessageDialog(this, "Error esta intentando vender mas unidades de las que posee",
+								"Error", JOptionPane.ERROR_MESSAGE);
+					}
+				} else if (tipo.equals("C")) {
+					if (Double.parseDouble(ventanaAgregar.darUnidades()) <= 0
+							|| Double.parseDouble(ventanaAgregar.darPrecioUnidad()) <= 0
+							|| Double.parseDouble(ventanaAgregar.darPrecioUnidad()) <= 0) {
+						throw new Exception("Señor Stark... no me quiero ir....");
+					}
+					int numero = 1;
+					double unidades = Double.parseDouble(ventanaAgregar.darUnidades());
+					double valorUnidades = Double.parseDouble(ventanaAgregar.darPrecioUnidad());
+					double valorTotal = Double.parseDouble(ventanaAgregar.darTotal());
+					mundo.registrarCompra(unidades, valorUnidades, valorTotal);
+					panelKardex.addInformacion(ventanaAgregar.darUnidades(), ventanaAgregar.darPrecioUnidad(),
+							ventanaAgregar.darTotal(), numero, "C", mundo.getSaldoUnidades() + "",
+							mundo.getSaldoValorUnidad() + "", mundo.getSaldoValorTotal() + "");
+
+					numero++;
+					this.setVisible(true);
+					ventanaAgregar.setVisible(false);
+					ventanaAgregar.limpiar();
+				}
+			} else if (comando.equals("C")) {
 				this.setVisible(true);
 				ventanaAgregar.setVisible(false);
 				ventanaAgregar.limpiar();
-			} else if (tipo.equals("V")) {
-				int numero = 1;
-				double unidades = Double.parseDouble(ventanaAgregar.darUnidades());
-				double valorUnidades = Double.parseDouble(ventanaAgregar.darPrecioUnidad());
-				double valorTotal = Double.parseDouble(ventanaAgregar.darTotal());
-				mundo.registrarVenta(unidades, valorUnidades, valorTotal);				
-				panelKardex.addInformacion(ventanaAgregar.darUnidades(), ventanaAgregar.darPrecioUnidad(),
-						ventanaAgregar.darTotal(), numero, "V", mundo.getSaldoUnidades()+"", mundo.getSaldoValorUnidad()+"", mundo.getSaldoValorTotal()+"");
-				
-				numero++;
-				this.setVisible(true);
-				ventanaAgregar.setVisible(false);
-				ventanaAgregar.limpiar();
-			} else if (tipo.equals("C")) {
-				int numero = 1;
-				double unidades = Double.parseDouble(ventanaAgregar.darUnidades());
-				double valorUnidades = Double.parseDouble(ventanaAgregar.darPrecioUnidad());
-				double valorTotal = Double.parseDouble(ventanaAgregar.darTotal());
-				mundo.registrarCompra(unidades, valorUnidades, valorTotal);
-				panelKardex.addInformacion(ventanaAgregar.darUnidades(), ventanaAgregar.darPrecioUnidad(),
-						ventanaAgregar.darTotal(), numero, "C", mundo.getSaldoUnidades()+"", mundo.getSaldoValorUnidad()+"", mundo.getSaldoValorTotal()+"");
-				
-				numero++;
-				this.setVisible(true);
-				ventanaAgregar.setVisible(false);
-				ventanaAgregar.limpiar();
+			} else if (comando.equals("In")) {
+				this.setVisible(false);
+				tipo = "I";
+				ventanaAgregar.setVisible(true);
+			} else if (comando.equals("VEN")) {
+				this.setVisible(false);
+				tipo = "V";
+				ventanaAgregar.setVisible(true);
+			} else if (comando.equals("COM")) {
+				this.setVisible(false);
+				tipo = "C";
+				ventanaAgregar.setVisible(true);
+			} else if (comando.equals("Vol")) {
+				this.setVisible(false);
+				KardexView prin = new KardexView();
+				prin.setVisible(true);
 			}
-		} else if (comando.equals("C")) {
-			this.setVisible(true);
-			ventanaAgregar.setVisible(false);
-			ventanaAgregar.limpiar();
-		} else if (comando.equals("In")) {
-			this.setVisible(false);
-			tipo = "I";
-			ventanaAgregar.setVisible(true);
-		} else if (comando.equals("VEN")) {
-			this.setVisible(false);
-			tipo = "V";
-			ventanaAgregar.setVisible(true);
-		} else if (comando.equals("COM")) {
-			this.setVisible(false);
-			tipo = "C";
-			ventanaAgregar.setVisible(true);
-		} else if (comando.equals("Vol")){
-			this.setVisible(false);
-			KardexView prin = new KardexView();
-			prin.setVisible(true);
+		} catch (NumberFormatException e1) {
+			JOptionPane.showMessageDialog(this, "Error solo se aceptan valores numericos");
+		} catch (Exception eerr) {
+			if (eerr.getMessage().equals("Señor Stark... no me quiero ir....")) {
+				JOptionPane.showMessageDialog(ventanaAgregar, "No se permite ingresar valores negativos");
+			}
 		}
-		
+
 	}
 }
--- ProyectoKardex/src/kardex/view/VistaPP.java
+++ ProyectoKardex/src/kardex/view/VistaPP.java
@@ -13,17 +13,17 @@ import kardex.logic.LogicKardex;
 public class VistaPP extends JFrame implements ActionListener {
 
 	private PanelKardexPP panelKardexPP;
-	
+
 	private FrameObtenerDatosPP ventanaAgregarPP;
-	
+
 	private LogicKardex mundo;
-	
+
 	private PanelOpcionesAgregarPP panelBotonesPP;
-	
+
 	private String tipo;
-	
+
 	private int numCompra = 1;
-	
+
 	private int numVenta = 1;
 
 	public VistaPP() {
@@ -40,75 +40,99 @@ public class VistaPP extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		try{
-		String comando = e.getActionCommand();
-		if (comando.equals("A")) {
-			if (tipo.equals("I")) {
-				if(Double.parseDouble(ventanaAgregarPP.darUnidades())<=0 || Double.parseDouble(ventanaAgregarPP.darPrecioUnidad())<=0
-						|| Double.parseDouble(ventanaAgregarPP.darPrecioUnidad())<=0){
-					throw new Exception("Señor Stark... no me quiero ir....");
-				}
-				mundo.setSaldoUnidades(Double.parseDouble(ventanaAgregarPP.darUnidades()));
-				mundo.setSaldoValorUnidad(Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()));
-				mundo.setSaldoValorTotal(Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()));
-				panelKardexPP.addInformacion(ventanaAgregarPP.darUnidades(), ventanaAgregarPP.darPrecioUnidad(),
-						ventanaAgregarPP.darTotal(), 0, "I", "", "", "");
-				this.setVisible(true);
-				ventanaAgregarPP.setVisible(false);
-				ventanaAgregarPP.limpiar();
-				panelBotonesPP.quitarOpor();
-			} 
-			else if (tipo.equals("V")) {
-				
-				double unidades = Double.parseDouble(ventanaAgregarPP.darUnidades());
-				double valorUnidades = Double.parseDouble(ventanaAgregarPP.darPrecioUnidad());
-				double valorTotal = Double.parseDouble(ventanaAgregarPP.darTotal());
-				mundo.registrarVenta(unidades, valorUnidades, valorTotal);
-				panelKardexPP.addInformacion(ventanaAgregarPP.darUnidades(), ventanaAgregarPP.darPrecioUnidad(),
-						ventanaAgregarPP.darTotal(), numVenta, "V",mundo.getSaldoUnidades()+"", mundo.getSaldoValorUnidad()+"", mundo.getSaldoValorTotal()+"");
-				
-				numVenta++;
-				this.setVisible(true);
-				ventanaAgregarPP.setVisible(false);
-				ventanaAgregarPP.limpiar();
-			} else if (tipo.equals("C")) {
-				double unidades = Double.parseDouble(ventanaAgregarPP.darUnidades());
-				double valorUnidades = Double.parseDouble(ventanaAgregarPP.darPrecioUnidad());
-				double valorTotal = Double.parseDouble(ventanaAgregarPP.darTotal());
-				 mundo.registrarCompra(unidades, valorUnidades, valorTotal);
-				panelKardexPP.addInformacion(ventanaAgregarPP.darUnidades(), ventanaAgregarPP.darPrecioUnidad(),
-						ventanaAgregarPP.darTotal(), numCompra, "C", mundo.getSaldoUnidades()+"", mundo.getSaldoValorUnidad()+"", mundo.getSaldoValorTotal()+"");
+		try {
+			String comando = e.getActionCommand();
+			if (comando.equals("A")) {
+				if (tipo.equals("I")) {
+					if (Double.parseDouble(ventanaAgregarPP.darUnidades()) <= 0
+							|| Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()) <= 0
+							|| Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()) <= 0) {
+						throw new Exception("Señor Stark... no me quiero ir....");
+					}
+					mundo.setSaldoUnidades(Double.parseDouble(ventanaAgregarPP.darUnidades()));
+					mundo.setSaldoValorUnidad(Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()));
+					mundo.setSaldoValorTotal(Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()));
+					panelKardexPP.addInformacion(ventanaAgregarPP.darUnidades(), ventanaAgregarPP.darPrecioUnidad(),
+							ventanaAgregarPP.darTotal(), 0, "I", "", "", "");
+					this.setVisible(true);
+					ventanaAgregarPP.setVisible(false);
+					ventanaAgregarPP.limpiar();
+					panelBotonesPP.quitarOpor();
+				} else if (tipo.equals("V")) {
+					if (Double.parseDouble(ventanaAgregarPP.darUnidades()) <= 0
+							|| Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()) <= 0
+							|| Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()) <= 0) {
+						throw new Exception("Señor Stark... no me quiero ir....");
+					}
+					try {
+						double unidades = Double.parseDouble(ventanaAgregarPP.darUnidades());
+						double valorUnidades = Double.parseDouble(ventanaAgregarPP.darPrecioUnidad());
+						double valorTotal = Double.parseDouble(ventanaAgregarPP.darTotal());
+						if (unidades > mundo.getSaldoUnidades()) {
+							throw new Exception();
+						}
 
-				numCompra++;
+						mundo.registrarVenta(unidades, valorUnidades, valorTotal);
+						panelKardexPP.addInformacion(ventanaAgregarPP.darUnidades(), ventanaAgregarPP.darPrecioUnidad(),
+								ventanaAgregarPP.darTotal(), numVenta, "V", mundo.getSaldoUnidades() + "",
+								mundo.getSaldoValorUnidad() + "", mundo.getSaldoValorTotal() + "");
+
+						numVenta++;
+						this.setVisible(true);
+						ventanaAgregarPP.setVisible(false);
+						ventanaAgregarPP.limpiar();
+					} catch (Exception e1) {
+						JOptionPane.showMessageDialog(this,
+								"Error esta intentando vender mas unidades de las que posee", "Error",
+								JOptionPane.ERROR_MESSAGE);
+					}
+				} else if (tipo.equals("C")) {
+					if (Double.parseDouble(ventanaAgregarPP.darUnidades()) <= 0
+							|| Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()) <= 0
+							|| Double.parseDouble(ventanaAgregarPP.darPrecioUnidad()) <= 0) {
+						throw new Exception("Señor Stark... no me quiero ir....");
+					}
+					double unidades = Double.parseDouble(ventanaAgregarPP.darUnidades());
+					double valorUnidades = Double.parseDouble(ventanaAgregarPP.darPrecioUnidad());
+					double valorTotal = Double.parseDouble(ventanaAgregarPP.darTotal());
+					mundo.registrarCompra(unidades, valorUnidades, valorTotal);
+					panelKardexPP.addInformacion(ventanaAgregarPP.darUnidades(), ventanaAgregarPP.darPrecioUnidad(),
+							ventanaAgregarPP.darTotal(), numCompra, "C", mundo.getSaldoUnidades() + "",
+							mundo.getSaldoValorUnidad() + "", mundo.getSaldoValorTotal() + "");
+
+					numCompra++;
+					this.setVisible(true);
+					ventanaAgregarPP.setVisible(false);
+					ventanaAgregarPP.limpiar();
+				}
+			} else if (comando.equals("C")) {
 				this.setVisible(true);
 				ventanaAgregarPP.setVisible(false);
 				ventanaAgregarPP.limpiar();
+			} else if (comando.equals("In")) {
+				this.setVisible(false);
+				tipo = "I";
+				ventanaAgregarPP.setVisible(true);
+			} else if (comando.equals("VEN")) {
+				this.setVisible(false);
+				tipo = "V";
+				ventanaAgregarPP.setVisible(true);
+			} else if (comando.equals("COM")) {
+				this.setVisible(false);
+				tipo = "C";
+				ventanaAgregarPP.setVisible(true);
+			} else if (comando.equals("Vol")) {
+				this.setVisible(false);
+				KardexView prin = new KardexView();
+				prin.setVisible(true);
 			}
-		} else if (comando.equals("C")) {
-			this.setVisible(true);
-			ventanaAgregarPP.setVisible(false);
-			ventanaAgregarPP.limpiar();
-		} else if (comando.equals("In")) {
-			this.setVisible(false);
-			tipo = "I";
-			ventanaAgregarPP.setVisible(true);
-		} else if (comando.equals("VEN")) {
-			this.setVisible(false);
-			tipo = "V";
-			ventanaAgregarPP.setVisible(true);
-		} else if (comando.equals("COM")) {
-			this.setVisible(false);
-			tipo = "C";
-			ventanaAgregarPP.setVisible(true);
-		} else if (comando.equals("Vol")){
-			this.setVisible(false);
-			KardexView prin = new KardexView();
-			prin.setVisible(true);
+
+		} catch (NumberFormatException x) {
+			JOptionPane.showMessageDialog(this, "Error solo se aceptan valores numericos");
 		}
-		
-		
-		}catch(Exception eerr){
-			if(eerr.getMessage().equals("Señor Stark... no me quiero ir....")){
+
+		catch (Exception eerr) {
+			if (eerr.getMessage().equals("Señor Stark... no me quiero ir....")) {
 				JOptionPane.showMessageDialog(ventanaAgregarPP, "No se permite ingresar valores negativos");
 			}
 		}
